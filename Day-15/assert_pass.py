b97087499b12045d828ac07e08014b7857a3764a
Likitha-Bindu-Mallela <likithabindumallela@gmail.com>
2026-09-05 17:37:36 +0530

    assert_pass patterns

---
+++ Day-15/assert_pass.py
@@ -0,0 +1,13 @@
+#pass - to define a empty block of code 
+#assert - used for debugging(finding the errors )  / development purpose , creating own errors
+
+
+a=5
+if a>1:
+    pass
+
+i%=0
+for i in range(i):
+    pass    
+
+    
---
+++ Day-15/patterns.py
@@ -0,0 +1,55 @@
+for i in range(7):
+    for j in range(10):
+        print("*", end=' ')
+    print()   
+
+
+
+for row in range(5):
+    for col in range(5):
+        print('X', end=' ')
+    print()        
+
+
+for row in range(5):
+    for col in range(5):
+        print(row,end=' ')
+    print()     
+
+
+for row in range(5):
+    for col in range(5):
+        print(col,end=' ')
+    print()      
+
+
+for i in range(5):
+    for j in range(5):
+        print(i+j,end= ' ')
+    print()    
+
+
+for i in range(7):
+    for j in range(5):
+        print(j%2,end=' ')
+    print()    
+
+
+for i in range(5):
+    for j in range(5):
+        print((i+j)%2,end=' ')
+    print()   
+
+
+for i in range(5):
+    for j in range(i+1):
+        print('X',end=' ')
+    print()        
+
+
+for i in range(5):
+    for j in range(5-i):
+        print('*', end=' ')
+    print()    
+
+
