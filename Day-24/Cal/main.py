55140b3338790078b88334949aec2d21dd8b2d9e
Likitha-Bindu-Mallela <likithabindumallela@gmail.com>
2026-09-05 17:48:51 +0530

    ATM Cal

---
+++ Day-24/Cal/main.py
@@ -0,0 +1,37 @@
+import logic 
+
+logic.add(2,3)
+logic.sub(2,3)
+logic.mul(2,3)
+logic.div(2,3)
+logic.exp(2,3)
+logic.rem(2,3)
+
+
+#recommended one
+
+import logic as lg
+
+lg.add(2,3)
+lg.sub(2,3)
+lg.mul(2,3)
+lg.div(2,3)
+lg.exp(2,3)
+lg.rem(2,3)
+
+
+from logic import add,sub
+
+add(4,5)
+sub(6,3)
+
+from logic import *
+
+add(4,5)
+sub(4,5)
+mul(4,5)
+div(4,5)
+exp(4,5)
+rem(4,5)
+
+
