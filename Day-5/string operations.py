438ac7cf6a6be87323d0de80074ef7e504a686b3
Likitha-Bindu-Mallela <likithabindumallela@gmail.com>
2026-09-05 16:24:17 +0530

    input formatting string operations

---
+++ Day-5/string operations.py
@@ -0,0 +1,64 @@
+Python 3.9.10 (tags/v3.9.10:f2f3f53, Jan 17 2022, 15:14:21) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> s=''
+>>> s
+''
+>>> s='codegyan'
+>>> s
+'codegyan'
+>>> 'codegyan' + 'PFS'
+'codegyanPFS'
+>>> 'codegyan'*9
+'codegyancodegyancodegyancodegyancodegyancodegyancodegyancodegyancodegyan'
+>>> '_*_'*15
+'_*__*__*__*__*__*__*__*__*__*__*__*__*__*__*_'
+>>> '*'*10
+'**********'
+>>> s = 'codegyan'
+>>> s[4]
+'g'
+>>> s[-1]
+'n'
+>>> s[6]
+'a'
+>>> s[-2]
+'a'
+>>> names = 'likitha' 'sushma' 'navya'
+>>> names[0]
+'l'
+>>> names[7]
+'s'
+>>> names[-5]
+'n'
+>>> names[-3]
+'v'
+>>> #s[start:end+1:step]=>s[0:len:1]
+>>> names(:7)
+SyntaxError: invalid syntax
+>>> names(0:7)
+SyntaxError: invalid syntax
+>>> names[0:7]
+'likitha'
+>>> names[8:14]
+'ushman'
+>>> names[16:20]
+'ya'
+>>> names[-1:-6]
+''
+>>> names[-1:-6:-1]
+'ayvan'
+>>> names(::-1)
+SyntaxError: invalid syntax
+>>> names[::-1]
+'ayvanamhsusahtikil'
+>>> names[::2]
+'lktauhaay'
+>>> 'likitha' in names
+True
+>>> 'navya' in names
+True
+>>> 'chandra' not in names
+True
+>>> Type "help", "copyright", "credits" or "license()" for more information.
+SyntaxError: invalid syntax
+>>> #string operations -> concentation -> combining two elements , reputation, slicing , indexing-> accessing specific element
